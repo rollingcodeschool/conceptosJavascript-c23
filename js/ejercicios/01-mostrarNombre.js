978ad111b2ae40ae4582e5fd434de6763b37cfc9
Emilse Arias <emilsearias@gmail.com>
2026-03-02 11:57:37 -0300

    agregue el ejercicio de mostrar nombres usando prompt

---
+++ js/ejercicios/01-mostrarNombre.js
@@ -0,0 +1,13 @@
+//Como pedir un dato al usuario y mostrarlo por pantalla
+const nombre = prompt('Ingresa tu nombre')
+const apellido = prompt('Ingresa tu apellido')
+
+console.log(nombre)
+console.log(apellido)
+
+//mostrar un mensaje al usuario
+// alert('Tu nombre y apellido es: ' + nombre + ', ' + apellido)
+
+//document.writeln('Tu nombre y apellido es: ' + nombre + ', ' + apellido)
+document.writeln('<p>Tu nombre y apellido es: ' + nombre + ', ' + apellido + '</p>')
+document.writeln(`Tu nombre y apellido es: ${nombre}, ${apellido} `)

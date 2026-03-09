f3584da7cd7023cc87dccbc8a4c2592ac2e4387f
Emilse Arias <emilsearias@gmail.com>
2026-03-09 09:34:13 -0300

    agregamos un ejemplo de multiples condicionales anidados

---
+++ js/03-switch.js
@@ -0,0 +1,18 @@
+// Cree un programa al estilo de un cajero automatico con las siguientes opciones:
+// 1- consultar el saldo
+// 2- ingresar dinero
+// 3- extraer dinero
+
+const opcion = prompt(
+  "Ingresa una opción: 1-consultar el saldo - 2-ingresar dinero - 3-extraer dinero ",
+);
+
+// if (opcion === "1") {
+//   alert("consultar el saldo");
+// } else if (opcion === "2") {
+//   alert("Aqui tengo que ingresar el dinero");
+// } else if (opcion === "3") {
+//   alert("Aqui tengo que extraer el dinero");
+// } else {
+//   alert("Ingresaste una opción erronea");
+// }

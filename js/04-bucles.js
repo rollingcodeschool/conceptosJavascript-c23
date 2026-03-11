050515524adf1112401a3cdceeec9aadda028711
Emilse Arias <emilsearias@gmail.com>
2026-03-11 09:40:08 -0300

    agregamos tres ejemplos pequeños para ver la sintaxis de los bucles while, do-while y for

--- js/04-bucles.js
+++ js/04-bucles.js
@@ -2,7 +2,7 @@
 
 const pantalla = document.getElementById("pantalla");
 
-let contador = 100;
+// let contador = 1;
 
 // while( contador <= 50){
 //     //aqui agregar todas las lineas de codigo que quiero repetir 50 veces
@@ -10,7 +10,21 @@ let contador = 100;
 //     contador++ //contador = contador + 1
 // }
 
-do {
-  pantalla.innerHTML += `<p>Fila ${contador}</p>`;
-  contador++; //contador = contador + 1
-} while (contador <= 50);
+// do {
+//   pantalla.innerHTML += `<p>Fila ${contador}</p>`;
+//   contador++; //contador = contador + 1
+// } while (contador <= 50);
+
+
+// for(let contador=1; contador <=50; contador++ ){
+//   // aqui agrego todas las lineas de codigo que queremos repetir varias veces
+//   pantalla.innerHTML += `<p>Fila ${contador}</p>`
+// }
+
+// const prueba = confirm('¿Queres realizar esta operación nuevamente?')
+// console.log(prueba)
+
+while(confirm('¿Queres realizar esta operación?')){
+pantalla.innerHTML += `<p>Fila creada con confirm</p>`
+console.log('prueba de confirm')
+}

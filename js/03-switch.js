60afd65d29772216dc0108879bb3db9a296fcfca
Emilse Arias <emilsearias@gmail.com>
2026-03-11 10:09:20 -0300

    modificamos el ejercicio del cajero automatico con un bucle y vimos el concepto de scope

--- js/03-switch.js
+++ js/03-switch.js
@@ -2,12 +2,60 @@
 // 1- consultar el saldo
 // 2- ingresar dinero
 // 3- extraer dinero
-
-const opcion = prompt(
-  "Ingresa una opción: 1-consultar el saldo - 2-ingresar dinero - 3-extraer dinero ",
-);
 let saldo = 10000;
 
+do {
+  const opcion = prompt(
+    "Ingresa una opción: 1-consultar el saldo - 2-ingresar dinero - 3-extraer dinero ",
+  );
+  let pruebaScope = "ambito local";
+  console.log(pruebaScope);
+  switch (opcion) {
+    case "saldo":
+    case "1":
+    case 1:
+      //aqui agrego todas las lineas de codigo que necesito si la opción ingresada por el usuario es1
+      alert("Tu saldo actual es $" + saldo);
+
+      break;
+    case "2":
+    case 2:
+    case "deposito":
+      //aqui agrego todas las lineas de codigo que necesito si la opción ingresada por el usuario es 2
+      const deposito = parseFloat(
+        prompt("Ingresa el monto que deseas depositar"),
+      );
+      saldo = saldo + deposito; // saldo += deposito
+      alert(`Ingresaste $${deposito}, tu saldo actual es $${saldo}`);
+      break;
+    case "3":
+    case "extraer":
+      //aqui agrego todas las lineas de codigo que necesito si la opción ingresada por el usuario es 3
+      const extraccion = parseFloat(prompt("Ingresa el monto a extraer"));
+      //verificar si el usuario puede extraer el monto solicitado
+      if (extraccion <= saldo) {
+        saldo = saldo - extraccion;
+        alert(`Retiraste $${extraccion}, tu saldo actual es $${saldo}`);
+      } else {
+        alert("Fondos insuficientes");
+      }
+
+      console.log(pruebaScope, "cuando extraigo");
+      break;
+    default:
+      //aqui agrego todas las lineas de codigo que necesito si la opción ingresada por el usuario es 3
+      alert("Ingresaste una opción erronea");
+  }
+} while (confirm("¿Queres realizar otra operación?"));
+
+let pruebaScope = "nuevo ejemplo de prueba scope";
+console.log(pruebaScope);
+
+console.log(pruebaVar);
+var pruebaVar = 'hola mundo'
+console.log(pruebaVar);
+
+// ! ejemplo de bucles anidados que debemos evitar
 // if (opcion === "1") {
 //   alert("consultar el saldo");
 // } else if (opcion === "2") {
@@ -17,39 +65,3 @@ let saldo = 10000;
 // } else {
 //   alert("Ingresaste una opción erronea");
 // }
-
-switch (opcion) {
-  case "saldo":
-  case "1":
-  case 1:
-    //aqui agrego todas las lineas de codigo que necesito si la opción ingresada por el usuario es1
-    alert("Tu saldo actual es $" + saldo);
-    break;
-  case "2":
-  case 2:
-  case "deposito":
-    //aqui agrego todas las lineas de codigo que necesito si la opción ingresada por el usuario es 2
-    const deposito = parseFloat(
-      prompt("Ingresa el monto que deseas depositar"),
-    );
-    saldo = saldo + deposito; // saldo += deposito
-    alert(`Ingresaste $${deposito}, tu saldo actual es $${saldo}`);
-    break;
-  case "3":
-  case "extraer":
-    //aqui agrego todas las lineas de codigo que necesito si la opción ingresada por el usuario es 3
-    const extraccion = parseFloat(prompt("Ingresa el monto a extraer"));
-    //verificar si el usuario puede extraer el monto solicitado
-    if (extraccion <= saldo) {
-      saldo = saldo - extraccion;
-      alert(`Retiraste $${extraccion}, tu saldo actual es $${saldo}`);
-    } else {
-      alert("Fondos insuficientes");
-    }
-    break;
-  default:
-    //aqui agrego todas las lineas de codigo que necesito si la opción ingresada por el usuario es 3
-    alert("Ingresaste una opción erronea");
-}
-
-console.log("aqui termino la ejecucion del switch");

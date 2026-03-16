d79f762170feffb58aef6103fe90dc3dfdd08f84
Emilse Arias <emilsearias@gmail.com>
2026-03-16 11:08:12 -0300

    agregamos un ejemplo de funcion con valor de retorno

--- js/05-funciones.js
+++ js/05-funciones.js
@@ -3,15 +3,22 @@ function saludar() {
     const pantalla = document.getElementById('pantalla');
     pantalla.innerHTML += '<p>Hola mundo 🌎 </p>'
 } 
-
+// funcion con parametros
 function saludarPersona(persona, edad = 20 ){
     const pantalla = document.getElementById('pantalla');
     pantalla.innerHTML += `<p>Hola ${persona} </p>`
     console.log('Edad: ' + edad)
 }
 
-// funcion con parametros
-
+//funciones que retornan un valor
+function aplicarDescuento(precio, descuento){
+    const precioFinal = precio - ((precio * descuento)/100)    
+    // let precioFinal = ((precio * descuento)/100)  
+    // precioFinal = precio - precioFinal  
+    console.log(precioFinal)
+    return precioFinal
+    // nunca se debe agregar una linea de codigo debajo del return dentro de la funcion
+}
 
 console.log('hola')
 // invocar o llamar a la funcion
@@ -26,8 +33,6 @@ saludar()
 saludar()
 saludar()
 
-
-
 while(confirm('¿Queres saludar una persona?')){
     const nombre = prompt('Ingresa tu nombre')
     console.log(nombre)
@@ -35,4 +40,14 @@ while(confirm('¿Queres saludar una persona?')){
 }
 
 saludarPersona('Rodrigo', 25)
+
+const precio = Number(prompt('Ingresa un precio'))
+const descuento = Number(prompt('Ingresa un descuento'))
+
+const precioFinal = aplicarDescuento(precio, descuento)
+const pantalla = document.getElementById('pantalla')
+pantalla.innerHTML += `<p>El precio que ingresaste fue ${precio}, con el descuento de ${descuento}%, el precio final es ${precioFinal}</p>`
+
+
+pantalla.innerHTML += `<p>El precio que ingresaste fue $2000, con el descuento de 15%, el precio final es ${aplicarDescuento(2000,15)}</p>`
 
